bfcd356f5864304d69432a4fb505e6b27392130d
Kevin C. Escobedo <escobedo001@gmail.com>
2019-07-16 16:22:05 -0700

    Initial Commit

---
+++ vc_natures.py
@@ -0,0 +1,12 @@
+#Author: Kevin C. Escobedo
+#Email: escobedo001@gmail.com
+
+natures = {0: "Hardy", 1: "Lonely", 2: "Brave", 3: "Adamant", 4: "Naughty",
+           5: "Bold", 6: "Docile", 7: "Relaxed", 8: "Impish", 9: "Lax",
+           10: "Timid", 11: "Hasty", 12: "Serious", 13: "Jolly", 14: "Naïve",
+           15: "Modest", 16: "Mild", 17: "Quiet", 18: "Bashful", 19: "Rash",
+           20: "Calm", 21: "Gentle", 22: "Sassy", 23: "Careful", 24: "Quirky"}
+
+def calculate_nature(EXP: int) -> str:
+    '''Calculates what nature a VC Pokémon will have in modern games'''
+    return natures[EXP % 25]
---
+++ vc_natures_gui.py
@@ -0,0 +1,57 @@
+#Author: Kevin C. Escobedo
+#Email: escobedo001@gmail.com
+import vc_natures
+import tkinter
+import sys
+import os
+
+class Interface:
+    def __init__(self):
+        self.root_window = tkinter.Tk()
+        self.root_window.geometry("300x100")
+        self.root_window.iconbitmap(self.resource_path("pokeball.ico"))
+        self.root_window.resizable(False, False)
+        self.root_window.title("VC Nature Calculator")
+        self.EXP = tkinter.Entry(self.root_window, width = 30)
+        self.result = tkinter.StringVar()
+        self.nature_result = ""
+
+    def resource_path(self, relative_path):
+        '''Get absolute path to resource, works for dev and for PyInstaller'''
+        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base_path, relative_path)
+
+    def get_exp(self):
+        try:
+            exp = int(self.EXP.get())
+            if exp < 0:
+                raise ValueError
+            return exp
+        except ValueError:
+            self.result.set("Invalid EXP")
+
+
+    def calculate_nature(self):
+        try:
+            experience = self.get_exp()
+            self.nature_result = vc_natures.calculate_nature(experience)
+            self.result.set("{} Nature".format(self.nature_result))
+        except TypeError:
+            pass
+
+    def run(self):
+        '''Runs the GUI'''
+        tkinter.Label(self.root_window, text = "Enter EXP").grid(row=0, column=0)
+        self.EXP.grid(row=0, column=1)
+
+        calculate_button = tkinter.Button(self.root_window, text = "Calculate", command = self.calculate_nature)
+        calculate_button.grid(row = 1, column = 1)
+
+        
+        tkinter.Label(self.root_window, textvariable = self.result).grid(row = 2, column = 1)
+        self.result.set("{}".format(self.nature_result))
+
+        self.root_window.mainloop()
+
+if __name__ == "__main__":
+    Interface().run()
